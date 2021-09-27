cb8b1a192fa0c8286f1a96ad271ded423e864cc7
Vishal <vishalvk38879@gmail.com>
2021-09-27 12:30:26 +0530

    added readme and update fibonacci

--- fibonacci .py
+++ fibonacci .py
@@ -1,10 +1,20 @@
-# Print fibonacci number 
+# Print fibonacci number at that position give by user
 def fibo(n):
     if n==0:
         return 0
     elif n==1:
         return 1
     else :
-        return fibo(n-1)+fibo(n-2)
+        #for fibonacci number at n possition 
+        # return fibo(n-1)+fibo(n-2)
+    # for fibonacci series
+        a=0
+        b=1
+        print(f"{a},{b}", end='')
+        for i in range (2,n+1):
+            c=a+b
+            print(",{}".format(c), end='')
+            a,b = b,c
 numb= int(input("Enter a number "))
-print(fibo(numb))
+fibo(numb)
+# print(fibo(numb))
